75c7cb13cff3bf447f7d557c0b4a20b3fa6613bc
Enzo <p.manalaysay@itsolivetti.it>
2026-02-17 16:39:34 +0100

    feat: New Todos Component

---
+++ todo/src/app/todos/todos.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todos',
+  imports: [],
+  templateUrl: './todos.html',
+  styleUrl: './todos.css',
+  standalone: true
+})
+export class Todos {
+
+}
